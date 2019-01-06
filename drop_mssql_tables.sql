524352c4049cf614a86769e03fda4de5ff249e9b
varanasikalyan@icloud.com <varanasikalyan@icloud.com>
2019-01-06 12:21:17 +0530

    Upgraded api

--- drop_mssql_tables.sql
+++
@@ -1,7 +0,0 @@
-drop table dbo.namebasics
-drop table dbo.titleakas
-drop table dbo.titleepisodes
-drop table dbo.titlecrew
-drop table dbo.titlebasics
-drop table dbo.titleprincipals
-drop table dbo.titleratings
